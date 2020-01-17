dc28f8e18816e799421ff6b807d3c9016977be63
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-01-17 01:42:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,18 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(number)
-    sql = 
-  end   
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      ORDER BY students.id 
+      LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
+  end 
   
   def save
     sql = <<-SQL
